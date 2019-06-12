5d7190a5db7f7770dcefe8da24fb8698fa26fc16
Munoon <munoongg@gmail.com>
2019-06-12 15:42:29 +0300

    Added GPL license

--- csgo-rankme.php
+++ csgo-rankme.php
@@ -7,12 +7,30 @@
     if (!defined('ABSPATH')) exit;
 
     /*
-    Plugin Name: CSGO Rankme
-    Plugin URI: https://github.com/Munoon/CSGO-Rankme-Web-Interface-for-WordPress
-    Description: Plugin allow show information about players from database of plugin Rankme (by Kento). It is support scoreboard, search, players profile and using few databases.
-    Version: 1.0
-    Author: Munoon
-    Author URI: https://github.com/Munoon
+        Plugin Name: CSGO Rankme
+        Plugin URI: https://github.com/Munoon/CSGO-Rankme-Web-Interface-for-WordPress
+        Description: Plugin allow show information about players from database of plugin Rankme (by Kento). It is support scoreboard, search, players profile and using few databases.
+        Version: 1.0
+        Author: Munoon
+        Author URI: https://github.com/Munoon
+    */
+
+    /*
+        Copyright 2019 Munoon (email: munoongg@gmail.com)
+
+        This program is free software; you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation; either version 2 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program; if not, write to the Free Software
+        Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
     */
 
     register_activation_hook(__FILE__, 'rankme_createScoreboardTable');
